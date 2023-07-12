38e1b65e7cb69a3b74807ef1c67f9f36dca74e65
Stanislas Bernard <bernardstanislas@gmail.com>
2023-07-12 11:33:09 +0200

    :sparkles: Add simple form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,56 @@
   import {supabase} from "../lib"
 
   const entriesPromise =  supabase.from("entries").select();
-console.log(entriesPromise)
 
+  const handleSumbit = (event) => {
+      const formData = new FormData(event.target)
+      console.log(formData)
+      console.log(formData.get("quantity-in"))
+    }
 </script>
+<form on:submit|preventDefault={handleSumbit}>
+  <div>
+  <label>
+  Quantity in
+  <input name="quantity-in" type="number" />
+  </label>
+  </div>
+  <div>
+  <label>
+  Quantity out
+  <input name="quantity-out" type="number" />
+  </label>
+  </div>
+  <div>
+  <label>
+  Grain
+  <input name="grain" type="number" />
+  </label>
+  </div>
+  <div>
+  <label>
+  Extraction
+  <select name="good-extraction">
+    <option value="true">OK</option>
+    <option value="false">KO</option>
+  </select>
+  </label>
+  <label>
+  Puck
+  <select name="good-puck">
+    <option value="true">OK</option>
+    <option value="false">KO</option>
+  </select>
+  </label>
+  </div>
+  <div>
+  <label>
+  Observation
+  <textarea name="observation"></textarea>
+  </label>
+  </div>
+  <button type="submit">Save</button>
+</form>
 {#await entriesPromise then data}
 <ul>
 {#each data.data as row}
